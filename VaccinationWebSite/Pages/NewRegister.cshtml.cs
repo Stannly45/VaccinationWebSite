bc9de9a2a0bab05759f63bc7cdcdfce4fe6279a7
Stannly45 <1stanli15stm@gmail.com>
2022-08-28 17:42:20 -0400

    Patient Update

---
+++ VaccinationWebSite/Pages/NewRegister.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VaccinationWebSite.Pages
+{
+    public class NewRegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
