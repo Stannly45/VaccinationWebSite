8ae71043cdb82e32226bbd5e0f19987ac4a77f1e
JohnCR058 <johncr000@gmail.com>
2022-09-23 06:16:35 -0400

    Se aumento paginas de las tarjetas de inicio

---
+++ VaccinationWebSite/Pages/Hearth.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VaccinationWebSite.Pages
+{
+    public class HearthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ VaccinationWebSite/Pages/Lab.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VaccinationWebSite.Pages
+{
+    public class LabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
